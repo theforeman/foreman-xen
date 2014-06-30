a7651e32a1fa12dd091c40362b2570778a22e498
Ben Bettridge <bettridge.ben@gmail.com>
2014-07-01 01:01:28 +1000

    changed boot order for xen devices

--- app/models/foreman_xen/xenserver.rb
+++ app/models/foreman_xen/xenserver.rb
@@ -208,7 +208,7 @@ module ForemanXen
       vm = client.servers.new :name               => args[:name],
                               :affinity           => host,
                               :pv_bootloader      => '',
-                              :hvm_boot_params    => { :order => 'dn' },
+                              :hvm_boot_params    => { :order => 'dhn' },
                               :other_config       => other_config,
                               :memory_static_max  => mem_max,
                               :memory_static_min  => mem_min,
