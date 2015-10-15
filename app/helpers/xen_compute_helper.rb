022c5f0f898c339a0ed7a42afcd385b91b4dba23
bkreitch <bkreitch@bk47.com>
2015-10-15 14:07:28 +0300

    refs #12017 - additional fixes
    - fix compute attribute
    
    closes GH-27

--- app/helpers/xen_compute_helper.rb
+++ app/helpers/xen_compute_helper.rb
@@ -1,8 +1,8 @@
 module XenComputeHelper
   def compute_attribute_map(params, compute_resource, new)
-    if @controller_name == 'hosts'
+    if controller_name == 'hosts'
       attribute_map = hosts_controller_compute_attribute_map(params, compute_resource, new)
-    elsif @controller_name = 'compute_attributes'
+    elsif controller_name == 'compute_attributes'
       attribute_map = compute_resource_controller_attribute_map(params, compute_resource)
     end
     attribute_map
@@ -26,17 +26,21 @@ module XenComputeHelper
 
   def hosts_controller_compute_attribute_map(params, compute_resource, new)
     attribute_map = empty_attribute_map
-    if new_host?
+    if new_host?(new)
       compute_attributes = compute_resource.compute_profile_attributes_for(params['host']['compute_profile_id'])
       if compute_attributes['VBDs']
         attribute_map[:volume_size] = compute_attributes['VBDs']['physical_size'] ? compute_attributes['VBDs']['physical_size'] : nil
         attribute_map[:volume_selected] = compute_attributes['VBDs']['sr_uuid'] ? compute_attributes['VBDs']['sr_uuid'] : nil
       end
-      if compute_attributes['VIFS']
+      if compute_attributes['VIFs']
         attribute_map[:network_selected] = compute_attributes['VIFs']['print'] ? compute_attributes['VIFs']['print'] : nil
       end
       attribute_map[:template_selected_custom] = compute_attributes['custom_template_name'] ? compute_attributes['custom_template_name'] : nil
       attribute_map[:template_selected_builtin] = compute_attributes['builtin_template_name'] ? compute_attributes['custom_template_name'] : nil
+      attribute_map[:cpu_count] = compute_attributes['vcpus_max'] ? compute_attributes['vcpus_max'] : nil
+      attribute_map[:memory_min] = compute_attributes['memory_min'] ? compute_attributes['memory_min'] : nil
+      attribute_map[:memory_max] = compute_attributes['memory_max'] ? compute_attributes['memory_max'] : nil
+      attribute_map[:power_on] = compute_attributes['start'] ? compute_attributes['start'] : nil
     elsif params && params['host'] && params['host']['compute_attributes']
       if params['host']['compute_attributes']['VBDs']
         attribute_map[:volume_size] = (params['host']['compute_attributes']['VBDs']['physical_size']) ? params['host']['compute_attributes']['VBDs']['physical_size'] : nil
@@ -95,3 +99,4 @@ module XenComputeHelper
      :memory_max => nil,
      :power_on => nil}
   end
+end
