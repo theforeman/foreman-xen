1d89195ce8b1315310a182fd99a28be8e94f506f
Ben Bettridge <bbettridge@switch.tv>
2017-01-28 00:25:57 +1100

    Moved javascript for populating fields into its own file. Removed hard coded JS cache params and use data- attributes instead.

--- app/assets/javascripts/compute_resources/xenserver/cache_refresh.js
+++ app/assets/javascripts/compute_resources/xenserver/cache_refresh.js
@@ -1,19 +1,19 @@
-function refreshCache(attribute_name, compute_resource_id, on_success) {
+function refreshCache(item, on_success) {
     tfm.tools.showSpinner();
-    url = "/foreman_xen/cache/refresh";
+    attribute_name = $(item).data('attribute')
     data = {
         type: attribute_name,
-        compute_resource_id: compute_resource_id
+        compute_resource_id: $(item).data('compute-resource-id')
     }
     $.ajax({
             type:'post',
-            url: url,
+            url: $(item).data('url'),
             data: data,
             complete: function(){
                 tfm.tools.hideSpinner();
             },
             error: function(){
-                $.jnotify(__("Error refreshing cache for " + attribute_name), 'error', true);
+                notify(__("Error refreshing cache for " + attribute_name), 'error', true);
             },
             success: on_success
         })
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,2 @@
 Rails.application.config.assets.precompile += %w(compute_resources/xenserver/cache_refresh.js)
+Rails.application.config.assets.precompile += %w(compute_resources/xenserver/populate_fields.js)
