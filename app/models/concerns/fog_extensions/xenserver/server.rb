aeb6a72f63c0fa8be0188a83cb7000c90e2b378b
root <root@deb8template.mpdft.gov.br>
2017-11-20 18:09:08 -0200

    Fixing attr_accessor

--- app/models/concerns/fog_extensions/xenserver/server.rb
+++ app/models/concerns/fog_extensions/xenserver/server.rb
@@ -6,7 +6,7 @@ module FogExtensions
       include ActionView::Helpers::NumberHelper
 
       attr_accessor :start
-      attr_accessov .image_id
+      attr_accessor :image_id
       attr_accessor :memory_min, :memory_max, :builtin_template_name, :hypervisor_host
 
       def to_s
